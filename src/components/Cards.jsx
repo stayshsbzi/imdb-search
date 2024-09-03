421238996661abd3c34c56097e221b532e78cf96
setayesh <setayeshsabzi83@gamil.com>
2024-09-03 18:10:10 +0330

    create handleSubmit func for search movies

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,22 +7,41 @@ import axios from 'axios'
 const Cards = () => {
   const [imdbData, setImdbData] = useState()
 
-  useEffect(() => {
-    const fetch = () => {
-      const url = 'http://www.omdbapi.com/?apikey=938bec53&s=ring'
-      axios.get(url)
-        .then(res => setImdbData(res.data.Search))
-        .catch(err => console.error(err))
-    }
+  // useEffect(() => {
+  //   const fetch = () => {
+  //     const url = 'http://www.omdbapi.com/?apikey=938bec53&s=attack'
+  //     axios.get(url)
+  //       .then(res => setImdbData(res.data.Search))
+  //       .catch(err => console.error(err))
+  //   }
 
-    fetch()
-  }, [])
+  //   fetch()
+  // }, [])
 
-  console.log(imdbData)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchParams = e.target[0].value
+    const url = `http://www.omdbapi.com/?apikey=938bec53&s=${searchParams}`
+    axios.get(url)
+      .then(res => setImdbData(res.data.Search))
+      .catch(err => console.error(err))
+  }
 
 
   return (
     <main className='tablecontainer'>
+
+      <form onSubmit={event => handleSubmit(event)}>
+        <input type='text' placeholder='Search for movies...' />
+        <button type='submit'>Search</button>
+      </form>
+
+      {!imdbData &&
+        <p>
+          Please Search your desire movies...
+        </p>
+      }
 
       {imdbData && imdbData.map(data => (
         <Card image={data.Poster} movieDes={data.Title} type={data.Type} year={data.Year} />
